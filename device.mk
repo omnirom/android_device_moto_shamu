d19a9041437a9295daf84d43924523c0bf6df500
Ethan Yonker <dees_troy@teamw.in>
2014-12-11 14:49:58 -0500

    shamu: add TWRP configruation
    Change-Id: Ic72a921d3e52a2d919041a28d55cffd5913d1530

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@
 #
 # Everything in this directory will become public
 
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/twrp.fstab:recovery/root/etc/twrp.fstab
 
 PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.rc:root/init.shamu.rc \
