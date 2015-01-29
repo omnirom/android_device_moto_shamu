8fea59b0cbce7627e5cdf1429b8205a0a64d7683
Dheeraj Shetty <dshetty@codeaurora.org>
2015-01-29 12:28:28 -0800

    Adding policy for IMS and WPA for VoWIFI
    Change-Id: I6cc0d9fff942e877695927cb0f4cf0dd4ebd4370

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,7 @@ BOARD_SEPOLICY_UNION += \
         mpdecision.te \
         netd.te \
         netmgrd.te \
+        wpa.te \
         platform_app.te \
         property.te \
         property_contexts \
