c4b20ba2cd9cc54619a00ff203a2320d2851bfbf
zephiK <zephik@gmail.com>
2015-05-29 02:38:56 -0400

    Add WiFi Calling build props
    Change-Id: I749fe02f219390bea8e98d7038122a5edb0d0b61

--- device.mk
+++ device.mk
@@ -165,6 +165,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.isUsbOtgEnabled=1
 
+# WiFi calling
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1
+
 # Audio effects
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
