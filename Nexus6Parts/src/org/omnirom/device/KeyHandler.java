a7ccf76efa3473d8e9d3987c4afc9e006e5fa508
Jake Whatley <jacob.whatley91@gmail.com>
2015-07-01 17:18:26 -0400

    shamu: deviceparts: initial commit
      - Use Find7Parts
      - Strip everything except double tap wake gesture
      - Set sysfs path from powerhal
      - Use sepolicy from CM: https://github.com/CyanogenMod/android_device_moto_shamu/commit/6e7a62a20f99474efc5a047c17044b97b4e8bbf7
    
    Change-Id: I92a329b6b0dc6603168b8942b73a68c7bf7d5e69

---
+++ Nexus6Parts/src/org/omnirom/device/KeyHandler.java
@@ -0,0 +1,90 @@
+package org.omnirom.device;
+
+import android.app.ActivityManagerNative;
+import android.app.KeyguardManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.IAudioService;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManagerGlobal;
+
+import com.android.internal.os.DeviceKeyHandler;
+import com.android.internal.util.ArrayUtils;
+
+public class KeyHandler implements DeviceKeyHandler {
+
+    private static final String TAG = KeyHandler.class.getSimpleName();
+    private static final boolean DEBUG = false;
+    private static final int GESTURE_REQUEST = 1;
+
+    // Supported scancodes
+    private static final int KEY_DOUBLE_TAP = 61;
+
+    private static final int[] sSupportedGestures = new int[]{
+        KEY_DOUBLE_TAP
+    };
+
+    private final Context mContext;
+    private final PowerManager mPowerManager;
+    private WakeLock mGestureWakeLock;
+    private KeyguardManager mKeyguardManager;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+        mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "GestureWakeLock");
+    }
+
+    private void ensureKeyguardManager() {
+        if (mKeyguardManager == null) {
+            mKeyguardManager =
+                    (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
+        }
+    }
+
+    public boolean handleKeyEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_UP) {
+            return false;
+        }
+        if (DEBUG) Log.i(TAG, "scanCode=" + event.getScanCode());
+        boolean isKeySupported = ArrayUtils.contains(sSupportedGestures, event.getScanCode());
+        if (isKeySupported ) {
+            if (event.getScanCode() == KEY_DOUBLE_TAP && !mPowerManager.isScreenOn()) {
+                if (DEBUG) Log.i(TAG, "KEY_DOUBLE_TAP");
+                mPowerManager.wakeUp(SystemClock.uptimeMillis());
+                return true;
+            }
+        }
+        return isKeySupported;
+    }
+
+    private void startActivitySafely(Intent intent) {
+        intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        try {
+            UserHandle user = new UserHandle(UserHandle.USER_CURRENT);
+            mContext.startActivityAsUser(intent, null, user);
+        } catch (ActivityNotFoundException e) {
+            // Ignore
+        }
+    }
+}
+
