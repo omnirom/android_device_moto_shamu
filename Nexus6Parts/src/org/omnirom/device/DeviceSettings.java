5b028aa6072349def989f9089b6e52bcbee91f72
Jake Whatley <jacob.whatley91@gmail.com>
2015-07-01 17:18:26 -0400

    shamu: deviceparts: initial commit
      - Use Find7Parts
      - Strip everything except double tap wake gesture
      - Set sysfs path from powerhal
      - Use sepolicy from CM: https://github.com/CyanogenMod/android_device_moto_shamu/commit/6e7a62a20f99474efc5a047c17044b97b4e8bbf7
    
    Change-Id: I92a329b6b0dc6603168b8942b73a68c7bf7d5e69

---
+++ Nexus6Parts/src/org/omnirom/device/DeviceSettings.java
@@ -0,0 +1,57 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.TwoStatePreference;
+
+public class DeviceSettings extends PreferenceActivity  {
+
+    public static final String KEY_DOUBLE_TAP_SWITCH = "double_tap";
+
+    private TwoStatePreference mDoubleTapSwitch;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.main);
+
+        mDoubleTapSwitch = (TwoStatePreference) findPreference(KEY_DOUBLE_TAP_SWITCH);
+        mDoubleTapSwitch.setEnabled(DoubleTapSwitch.isSupported());
+        mDoubleTapSwitch.setChecked(DoubleTapSwitch.isEnabled(this));
+        mDoubleTapSwitch.setOnPreferenceChangeListener(new DoubleTapSwitch());
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
