086727734a7dd19b17434bf580fc613ef10faa46
Jake Whatley <jacob.whatley91@gmail.com>
2015-09-01 00:14:03 -0400

    shamu: bring closer to AOSP
    Inspired by humberos' similar commit for sony/shinano-common & sony/leo
    
    Change-Id: Ia86177098d97e36e683c96bc1dcb0cdfcb1fe6a2

--- omni_shamu.mk
+++ omni_shamu.mk
@@ -14,40 +14,64 @@
 # limitations under the License.
 #
 
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
+# Inherit base AOSP device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 # Inherit APNs list
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/moto/shamu/device.mk)
+# Nexus6Parts
+PRODUCT_PACKAGES += \
+    Nexus6Parts
 
-PRODUCT_NAME := omni_shamu
-PRODUCT_DEVICE := shamu
-PRODUCT_BRAND := google
-PRODUCT_MODEL := Nexus 6
-PRODUCT_MANUFACTURER := motorola
+# OTG
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.isUsbOtgEnabled=1
 
-# Kernel inline build
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
-TARGET_KERNEL_CONFIG := omni_shamu_defconfig
-TARGET_VARIANT_CONFIG := omni_shamu_defconfig
-TARGET_SELINUX_CONFIG := omni_shamu_defconfig
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.no_wait_for_card=1 \
+    persist.radio.sib16_support=1
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/shamu/shamu:5.1.1/LYZ28E/1914015:user/release-keys" \
-    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28E 1914015 release-keys"
+# Rich Communications Service is disabled in 5.1
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rcs.supported=0
 
 # SELinux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
-$(call inherit-product-if-exists, vendor/motorola/shamu/device-vendor.mk)
+# Sound
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true \
+    persist.audio.dualmic.config=endfire \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=false
 
+# Tethering: allow tethering without provisioning app
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# TWRP
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/twrp.fstab:recovery/root/etc/twrp.fstab
+
+# WiFi calling
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/shamu/shamu:5.1.1/LYZ28E/1914015:user/release-keys" \
+    PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28E 1914015 release-keys"
