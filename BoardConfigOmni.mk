f70f068984a4aeaa5335d6841c1c35d9c922bb04
Jake Whatley <jacob.whatley91@gmail.com>
2015-10-16 19:17:15 -0400

    cleanup kernel configs
    Change-Id: I8513cd43d4ded989eb602cec79f58307bc08c8ec

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -16,8 +16,6 @@
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
 TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_VARIANT_CONFIG := shamu_defconfig
-TARGET_SELINUX_CONFIG := shamu_defconfig
 
 # TWRP
 TW_THEME := portrait_hdpi
