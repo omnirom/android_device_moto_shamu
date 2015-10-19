2bf6f85ca84743b3c3b8264b2a0a690909fb6fed
Jake Whatley <jacob.whatley91@gmail.com>
2015-10-19 13:18:24 -0400

    inherit vendor/motorola
    and fail if it does not exist
    
    Change-Id: I5ac98c8d6d737b0512adb904969650bbed1701f2

--- omni_shamu.mk
+++ omni_shamu.mk
@@ -48,3 +48,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys" \
     PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
+
+$(call inherit-product, vendor/motorola/shamu/device-vendor.mk)
+
