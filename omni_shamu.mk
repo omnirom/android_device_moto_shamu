d66e5425a17d0d78b6b71f513d167a1d11392be9
Jake Whatley <jacob.whatley91@gmail.com>
2016-01-29 14:22:48 -0500

    shamu: extract-files: logic updates
    ref: https://github.com/CyanogenMod/android_device_moto_shamu
    
    and
    
    https://github.com/CyanogenMod/android_device_oneplus_bacon
    
    Change-Id: If5ae0c2da00fc48317bb57eb060cb06f32782075

--- omni_shamu.mk
+++ omni_shamu.mk
@@ -81,4 +81,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MMB29S/2489379:user/release-keys" \
     PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29S 2489379 release-keys"
 
-$(call inherit-product, vendor/motorola/shamu/device-vendor.mk)
+$(call inherit-product, vendor/motorola/shamu/shamu-vendor-blobs.mk)
