cfc510e11dbef0c3628310404386594183bcbc21
Jake Whatley <jacob.whatley91@gmail.com>
2017-01-25 22:08:25 -0500

    kernel: compile during buildtime
    ref: https://github.com/omnirom/android_device_google_seed/commit/2b5a3c38617c2b852a0fc020c58df1a004b1fb93
    
    for "missing toolchain"
    
    Change-Id: I5b4bae3b1e3fb1a9763ef0026fefa42c92595b2d

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,3 +1,10 @@
+# Inline kernel building
+TARGET_KERNEL_CONFIG := shamu_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/shamu
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+
 # TWRP
 TW_THEME := portrait_hdpi
 TW_INCLUDE_L_CRYPTO := true
