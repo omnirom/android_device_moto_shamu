d76b2ba1478ffefd798c740b50c4b1bf33fe32ee
Jake Whatley <jacob.whatley91@gmail.com>
2017-01-25 21:57:06 -0500

    shamu: initial bringup for Nougat
    Change-Id: I79dbf8acdb363335850c8ed7bb9662216a1e788e

---
+++ omni_shamu.mk
@@ -0,0 +1,18 @@
+# Inherit base AOSP device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/shamu/shamu:7.1.1/N6F26Q/3549652:user/release-keys" \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26Q 3549652 release-keys"
+
+# Inherit OmniROM parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
